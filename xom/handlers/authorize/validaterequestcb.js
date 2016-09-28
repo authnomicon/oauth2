4bf4dc5178e7f9728b15ca126fb99a7c342dfd9b
Jared Hanson <jaredhanson@gmail.com>
2016-09-28 13:32:05 -0700

    Add tests.

--- xom/handlers/authorize/validaterequestcb.js
+++ xom/handlers/authorize/validaterequestcb.js
@@ -16,46 +16,36 @@ exports = module.exports = function(directory) {
         return cb(new oauth2orize.AuthorizationError('Unknown client', 'unauthorized_client'));
       }
       if (!client.redirectURIs || !client.redirectURIs.length) {
-        // `redirectURIs` is an optional property, and may not be present on
-        // on clients that do not use flows with redirection.  Such clients are
-        // not authorized to use the `/authorize` endpoint.
+        // The client has not registered any redirection endpoints.  Such
+        // clients are not authorized to use the authorization endpoint.
+        //
+        // This enforcement is in place in order to prevent the authorization
+        // server from functioning as an open redirector.  Refer to Section
+        // 3.1.2.2 of RFC 6749 for further details.
         return cb(new oauth2orize.AuthorizationError('Client has no registered redirect URIs', 'unauthorized_client'));
       }
+      if (client.redirectURIs.length > 1 && !redirectURI) {
+        // If multiple redirection URIs have been registered, the client must
+        // include a redirection URI with the authorization request.  Refer to
+        // Section 3.1.2.3 of RFC 6749 for further details.
+        return cb(new oauth2orize.AuthorizationError('Missing required parameter: redirect_uri', 'invalid_request'));
+      }
+      if (redirectURI && client.redirectURIs.indexOf(redirectURI) == -1) {
+        return cb(new oauth2orize.AuthorizationError('Client not permitted to use redirect URI', 'unauthorized_client'));
+      }
       
-      // TODO: Implement 3.1.2.2.  Registration Requirements
       /*
-      The authorization server MUST require the following clients to
-      register their redirection endpoint:
-
-      o  Public clients.
-
-      o  Confidential clients utilizing the implicit grant type.
-      */
-      
-      // TODO: Implement 3.1.2.3.  Dynamic Configuration
-      /*
-      If multiple redirection URIs have been registered, if only part of
-      the redirection URI has been registered, or if no redirection URI has
-      been registered, the client MUST include a redirection URI with the
-      authorization request using the "redirect_uri" request parameter.
-      */
-      
-      // TODO: Validate this stuff
-      
-      
-      
-      // FIXME: Temp hack, remove
-      //return cb(null, client, redirectURI);
-      
       if (!redirectURI) {
-        // If the request did not explicitly specify a redirect URI, use the
-        // higest priority URI specified in the client's registration.
+        // The client did not include a redirection URI with the authorization
+        // request, and it has a single registered redirect URI.  The registered
+        // redirect URI will be used to respond to the authorization request.
         redirectURI = client.redirectURIs[0];
       } else if (client.redirectURIs.indexOf(redirectURI) == -1) {
         return cb(new oauth2orize.AuthorizationError('Client not permitted to use redirect URI', 'unauthorized_client'));
       }
+      */
       
-      return cb(null, client, redirectURI);
+      return cb(null, client, redirectURI || client.redirectURIs[0]);
     });
   };
 };
