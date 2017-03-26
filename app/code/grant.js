db113dfd8cbd6b19ba618209e630252dc41dc884
Jared Hanson <jaredhanson@gmail.com>
2017-03-26 11:15:58 -0700

    Dynamically load response modes.

--- app/code/grant.js
+++ app/code/grant.js
@@ -1,15 +1,28 @@
-exports = module.exports = function(issueCb) {
+exports = module.exports = function(container, issue, logger) {
   var oauth2orize = require('oauth2orize');
   
-  // TODO: Make modes pluggable
-  return oauth2orize.grant.code({
-    modes: { // query
-      form_post: require('oauth2orize-fprm'),
-      web_message: require('oauth2orize-wmrm')
-    }
-  }, issueCb);
+  
+  var modeDecls = container.specs('http://schemas.authnomicon.org/js/aaa/oauth2/response/mode')
+  return Promise.all(modeDecls.map(function(spec) { return container.create(spec.id); } ))
+    .then(function(plugins) {
+      var modes = {}
+        , name;
+      plugins.forEach(function(mode, i) {
+        name = modeDecls[i].a['@mode'];
+        modes[name] = mode;
+        logger.info('Loaded response mode for OAuth 2.0 authorization code flow: ' + name);
+      });
+      
+      return oauth2orize.grant.code({
+        modes: modes
+      }, issue);
+    });
 };
 
 exports['@implements'] = 'http://schemas.authnomicon.org/js/aaa/oauth2/grant';
 exports['@type'] = 'code';
-exports['@require'] = [ './issue/code' ];
+exports['@require'] = [
+  '!container',
+  './issue/code',
+  'http://i.bixbyjs.org/Logger'
+];
