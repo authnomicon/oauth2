dee9a2d58db2ce78ed978c86e22f951ef96ccff7
Jared Hanson <jaredhanson@gmail.com>
2017-04-04 15:40:00 -0700

    Handle prompt=none errors.

--- app/middleware/prompt.js
+++ app/middleware/prompt.js
@@ -1,8 +1,28 @@
 exports = module.exports = function(flows) {
+  var oauth2orize = require('oauth2orize');
+  
   
   return function prompt(req, res, next) {
-    var prompt = req.oauth2.info.prompt;
-    var options = req.oauth2.info;
+    var areq = req.oauth2.req
+      , prompt = req.oauth2.info.prompt
+      , error = 'interaction_required'
+      , options;
+      
+    if (areq.prompt && areq.prompt.indexOf('none') !== -1) {
+      switch (prompt) {
+      case 'login':
+        error = 'login_required';
+        break;
+      case 'consent':
+        error = 'consent_required';
+        break;
+        // TODO: Account selection
+      }
+      
+      return next(new oauth2orize.AuthorizationError('Interaction with user is required to proceed', error, null, 403));
+    }
+    
+    options = req.oauth2.info;
     delete options.prompt;
     options.state = req.oauth2.transactionID;
   
