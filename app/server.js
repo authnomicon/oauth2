02cf45a3ee0061097bbfe74f6567c38ca871d3bc
Jared Hanson <jaredhanson@gmail.com>
2017-07-14 18:39:58 -0700

    Clarify plugin loading in server.

--- app/server.js
+++ app/server.js
@@ -6,39 +6,53 @@ exports = module.exports = function(container, store, logger) {
     store: store
   });
   
-  var paramDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/request/parameters')
-    , grantDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/grant')
-    , exchangeDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/exchange')
-  
-  return Promise.all(paramDecls.map(function(spec) { return container.create(spec.id); } ))
-    .then(function(plugins) {
+  return Promise.resolve(server)
+    .then(function(server) {
       // Register request parameter extensions with the OAuth 2.0 server.
-      plugins.forEach(function(plugin, i) {
-        server.grant(plugin);
-        logger.info('Loaded OAuth 2.0 request parameters: ' + paramDecls[i].a['@name']);
-      });
+      var paramDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/request/parameters');
+    
+      return Promise.all(paramDecls.map(function(spec) { return container.create(spec.id); } ))
+        .then(function(plugins) {
+          plugins.forEach(function(plugin, i) {
+            server.grant(plugin);
+            logger.info('Loaded OAuth 2.0 request parameters: ' + paramDecls[i].a['@name']);
+          });
+        })
+        .then(function() {
+          return server;
+        });
     })
-    .then(function() {
-      return Promise.all(grantDecls.map(function(spec) { return container.create(spec.id); } ));
-    })
-    .then(function(plugins) {
+    .then(function(server) {
       // Register response types with the OAuth 2.0 server.
-      plugins.forEach(function(plugin, i) {
-        server.grant(grantDecls[i].a['@type'] || plugin.name, plugin);
-        logger.info('Loaded OAuth 2.0 response type: ' + (grantDecls[i].a['@type'] || plugin.name));
-      });
+      var grantDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/grant')
+    
+      return Promise.all(grantDecls.map(function(spec) { return container.create(spec.id); } ))
+        .then(function(plugins) {
+          plugins.forEach(function(plugin, i) {
+            server.grant(grantDecls[i].a['@type'] || plugin.name, plugin);
+            logger.info('Loaded OAuth 2.0 response type: ' + (grantDecls[i].a['@type'] || plugin.name));
+          });
+        })
+        .then(function() {
+          return server;
+        });
     })
-    .then(function() {
-      return Promise.all(exchangeDecls.map(function(spec) { return container.create(spec.id); } ));
-    })
-    .then(function(plugins) {
+    .then(function(server) {
       // Register grant types with the OAuth 2.0 server.
-      plugins.forEach(function(plugin, i) {
-        server.exchange(exchangeDecls[i].a['@type'] || plugin.name, plugin);
-        logger.info('Loaded OAuth 2.0 grant type: ' + (exchangeDecls[i].a['@type'] || plugin.name));
-      });
+      var exchangeDecls = container.components('http://schemas.authnomicon.org/js/aaa/oauth2/exchange')
+    
+      return Promise.all(exchangeDecls.map(function(spec) { return container.create(spec.id); } ))
+        .then(function(plugins) {
+          plugins.forEach(function(plugin, i) {
+            server.exchange(exchangeDecls[i].a['@type'] || plugin.name, plugin);
+            logger.info('Loaded OAuth 2.0 grant type: ' + (exchangeDecls[i].a['@type'] || plugin.name));
+          });
+        })
+        .then(function() {
+          return server;
+        });
     })
-    .then(function() {
+    .then(function(server) {
       return server;
     });
 }
