8f36e8b1d9b1fec0733af83c7bc00686cdf76260
Jared Hanson <jaredhanson@gmail.com>
2017-08-09 16:41:48 -0700

    Adjust interfaces.

--- app/http/middleware/errorhandler.js
+++ app/http/middleware/errorhandler.js
@@ -5,7 +5,7 @@ exports = module.exports = function(server) {
   };
 };
 
-exports['@implements'] = 'http://schemas.authnomicon.org/js/http/oauth2/middleware/errorHandler';
+exports['@implements'] = 'http://schemas.authnomicon.org/js/oauth2/http/middleware/errorHandler';
 exports['@require'] = [
   '../../server'
 ];
