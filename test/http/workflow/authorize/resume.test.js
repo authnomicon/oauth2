48df2b67fba3483598ac18f78d5db8a5320d79a7
Jared Hanson <jaredhanson@gmail.com>
2017-10-25 14:03:39 -0700

    Test coverage for authorize resume.

---
+++ test/http/workflow/authorize/resume.test.js
@@ -0,0 +1,40 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../../app/http/workflow/authorize/resume');
+
+
+describe('http/workflow/authorize/resume', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  describe('creating handler', function() {
+    var server = {
+      resume: function(){},
+      authorizationErrorHandler: function(){}
+    };
+    var processTransaction = function(){};
+    var completeTransaction = function(){};
+    var prompt = function(){};
+    var errorLogging = sinon.spy();
+    
+    var stub = sinon.stub(server, 'resume').returns([
+      function transactionLoader(req, res, next){},
+      function resume(req, res, next){}
+    ]);
+    var handler = factory(server, processTransaction, completeTransaction, prompt, errorLogging);
+    
+    it('should invoke Server#resume with callback', function() {
+      expect(stub).to.have.been.calledWithExactly(processTransaction, completeTransaction);
+    });
+    
+    it('should return handler', function() {
+      expect(handler).to.be.an('array');
+      expect(handler[1]).to.equal(prompt);
+    });
+  }); // creating handler
+  
+});
