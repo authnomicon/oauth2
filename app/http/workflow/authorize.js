eee6da97ed984561daa748f0b8284f2800c881ef
Jared Hanson <jaredhanson@gmail.com>
2018-03-22 08:10:00 -0700

    Update interface name.

--- app/http/workflow/authorize.js
+++ app/http/workflow/authorize.js
@@ -6,7 +6,7 @@ exports = module.exports = function(resume) {
   };
 };
 
-exports['@implements'] = 'http://i.bixbyjs.org/http/state/Prompt';
+exports['@implements'] = 'http://i.bixbyjs.org/http/state/State';
 exports['@name'] = 'oauth2-authorize';
 exports['@require'] = [
   './authorize/resume'
