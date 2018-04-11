d3e4509ce28af42b22e0c8287df9d1d2b7d57f43
Jared Hanson <jaredhanson@gmail.com>
2018-04-11 09:54:34 -0700

    Use req.state.destroy when destroying the state.

--- app/http/handlers/authorize/completetransaction.js
+++ app/http/handlers/authorize/completetransaction.js
@@ -1,6 +1,11 @@
 exports = module.exports = function() {
   
   return function completeTransaction(req, txn, cb) {
+    // Flag this here because oauth2orize.authorize or oauth2orize.resume hijackes the respone ending process, halting
+    // the middleware from proceeding if the transaction is complete.  Therefore we don't fall throuhg
+    // the ceremony the execute the finish phase.
+    req.state.complete();
+    
     return cb();
   };
 };
