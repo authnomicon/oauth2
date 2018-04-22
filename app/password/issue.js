0c9cc0c15f4f361e2bd8973356f41fc0fe4acda5
Jared Hanson <jaredhanson@gmail.com>
2018-04-22 15:17:10 -0700

    Put authorization request into password exchange.

--- app/password/issue.js
+++ app/password/issue.js
@@ -1,4 +1,4 @@
-exports = module.exports = function(issueToken, pdp, Resources, AAA, service, verifyPassword) {
+exports = module.exports = function(issueToken, aaa, pdp, Resources, AAA, service, verifyPassword) {
   var MFARequiredError = require('oauth2orize-mfa').MFARequiredError;
   //var klamm = require('klamm-oauth2');
   
@@ -11,6 +11,73 @@ exports = module.exports = function(issueToken, pdp, Resources, AAA, service, ve
     console.log(scope);
     console.log(body);
     console.log(authInfo);
+    
+    verifyPassword(username, passwd, function(err, user) {
+      console.log('PASSWORD VERIFIED');
+      console.log(err);
+      console.log(user);
+      
+      var audience = body.audience;
+      
+      var options = {
+        client: client,
+        user: user,
+        scope: scope,
+        audience: audience
+      };
+      
+      var req = aaa.request(options, function(res) {
+        console.log(res);
+      
+        function ondecision(result) {
+          if (result === true) {
+            var resource = { id: 'http://www.example.com/',
+             name: 'Example Service',
+             tokenTypes: 
+              [ { type: 'application/fe26.2' },
+                { type: 'urn:ietf:params:oauth:token-type:jwt',
+                  secret: 'some-shared-with-rs-s3cr1t-asdfasdfaieraadsfiasdfasd' } ] }
+        
+            //res.resources = [ resource ]
+            // TODO: Get res.resources here
+        
+            return cb(null, 'some-access-token-goes-here');
+        
+            //return cb(null, true, { permissions: [ { resource: resource, scope: 'foo' } ]});
+          } else {
+            return cb(null, false);
+          }
+        
+          // TODO: Handle indeterminte by prompting?  Or attenuating scope?
+        }
+      
+        function onprompt(name, options) {
+          // TODO: Send errors based on prompt.  Consent is implicit
+          
+          var opts = options || {};
+          opts.prompt = name;
+          return cb(null, false, opts);
+        }
+      
+        function onend() {
+          res.removeListener('decision', ondecision);
+          res.removeListener('prompt', onprompt);
+        }
+      
+        res.once('decision', ondecision);
+        res.once('prompt', onprompt);
+        res.once('end', onend);
+      });
+    
+      req.on('error', function(err) {
+        // TODO:
+      })
+    
+      req.send();
+    });
+    
+    return;
+    
     
     verifyPassword(username, passwd, function(err, user) {
       console.log(err);
@@ -141,6 +208,7 @@ exports = module.exports = function(issueToken, pdp, Resources, AAA, service, ve
 
 exports['@require'] = [
   '../util/issuetoken',
+  'http://schemas.authnomicon.org/js/aaa',
   'http://schema.modulate.io/js/aaa/PolicyDecisionPoint',
   'http://schemas.modulate.io/js/aaa/realms',
   'http://schemas.modulate.io/js/aaa',
