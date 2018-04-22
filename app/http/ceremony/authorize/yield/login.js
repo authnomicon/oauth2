28bc1b08ea02e6ab5ad1bad238af5751be330cb1
Jared Hanson <jaredhanson@gmail.com>
2018-04-22 16:18:57 -0700

    Silence verbose logging.

--- app/http/ceremony/authorize/yield/login.js
+++ app/http/ceremony/authorize/yield/login.js
@@ -1,11 +1,6 @@
 exports = module.exports = function() {
   
   function transition(req, res, next) {
-    console.log('YEILD TO OAUTH2');
-    console.log(req.state);
-    console.log(req.yieldState)
-    console.log(req.authInfo)
-    
     req.state.authN = req.state.authN || {
       methods: []
     };
@@ -23,8 +18,6 @@ exports = module.exports = function() {
   }
   
   function errorHandler(err, req, res, next) {
-    console.log('YEILD ERROR OAUTH2');
-    
     if (req.yieldState) {
       req.state.authN = req.state.authN || {};
       req.state.authN.failureCount =  req.yieldState.failureCount;
