e2a82bd54931eb9d55ba54e12a6aa32b2ac8958a
Jared Hanson <jaredhanson@gmail.com>
2018-05-04 11:38:36 -0700

    Add tests.

---
+++ test/http/ceremony/authorize/prompt.test.js
@@ -0,0 +1,20 @@
+/* global describe, it */
+
+var chai = require('chai');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../../app/http/ceremony/authorize/prompt');
+
+
+describe('http/ceremony/authorize/prompt', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.be.undefined;
+    expect(factory['@singleton']).to.be.undefined;
+  });
+  
+});
