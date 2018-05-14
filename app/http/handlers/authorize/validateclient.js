0c0a2fede13ef28dcae22ef45d5f18c23d25e785
Jared Hanson <jaredhanson@gmail.com>
2018-05-14 08:29:30 -0700

    Remove dead code and fix indentation.

--- app/http/handlers/authorize/validateclient.js
+++ app/http/handlers/authorize/validateclient.js
@@ -19,57 +19,42 @@ exports = module.exports = function(realms) {
   return function validateClient(clientID, redirectURI, cb) {
     
     realms.get(clientID, 'clients', function(err, client) {
-      console.log('GOT CLIENT!');
-      console.log(err);
-      console.log(client);
-      //return;
-    
-      //var dir = realm.createDirectory(function() {
-        // The directory is ready, continue processing by fetching the client
-        // record.
-        
-        //dir.get(clientID, function(err, client) {
-          if (err) { return cb(err); }
-          if (!client) {
-            return cb(new oauth2orize.AuthorizationError('Unknown client', 'unauthorized_client'));
-          }
-          if (!client.redirectURIs || !client.redirectURIs.length) {
-            // The client has not registered any redirection endpoints.  Such
-            // clients are not authorized to use the authorization endpoint.
-            //
-            // This enforcement is in place in order to prevent the authorization
-            // server from functioning as an open redirector.  Refer to Section
-            // 3.1.2.2 of RFC 6749 for further details.
-            return cb(new oauth2orize.AuthorizationError('Client has no registered redirect URIs', 'unauthorized_client'));
-          }
-          if (client.redirectURIs.length > 1 && !redirectURI) {
-            // If multiple redirection URIs have been registered, the client must
-            // include a redirection URI with the authorization request.  Refer to
-            // Section 3.1.2.3 of RFC 6749 for further details.
-            return cb(new oauth2orize.AuthorizationError('Missing required parameter: redirect_uri', 'invalid_request'));
-          }
-      
-      
-          if (redirectURI) {
-            var url = uri.parse(redirectURI);
-            //console.log(url);
-            if (url.protocol == 'storagerelay:') {
-              // TODO: Implement web/js origin checks
-              return cb(null, client, redirectURI, 'http://localhost:3001');
-            }
-          }
-      
-      
-          if (redirectURI && client.redirectURIs.indexOf(redirectURI) == -1) {
-            return cb(new oauth2orize.AuthorizationError('Client not permitted to use redirect URI', 'unauthorized_client'));
-          }
-      
-          return cb(null, client, redirectURI || client.redirectURIs[0], 'http://localhost:3001');
-        //});
-      //}); // realm.createDirectory(readyListener)
-      
-      // TODO: Handle dir.on('error')??
-      
+      if (err) { return cb(err); }
+      if (!client) {
+        return cb(new oauth2orize.AuthorizationError('Unknown client', 'unauthorized_client'));
+      }
+      if (!client.redirectURIs || !client.redirectURIs.length) {
+        // The client has not registered any redirection endpoints.  Such
+        // clients are not authorized to use the authorization endpoint.
+        //
+        // This enforcement is in place in order to prevent the authorization
+        // server from functioning as an open redirector.  Refer to Section
+        // 3.1.2.2 of RFC 6749 for further details.
+        return cb(new oauth2orize.AuthorizationError('Client has no registered redirect URIs', 'unauthorized_client'));
+      }
+      if (client.redirectURIs.length > 1 && !redirectURI) {
+        // If multiple redirection URIs have been registered, the client must
+        // include a redirection URI with the authorization request.  Refer to
+        // Section 3.1.2.3 of RFC 6749 for further details.
+        return cb(new oauth2orize.AuthorizationError('Missing required parameter: redirect_uri', 'invalid_request'));
+      }
+  
+  
+      if (redirectURI) {
+        var url = uri.parse(redirectURI);
+        //console.log(url);
+        if (url.protocol == 'storagerelay:') {
+          // TODO: Implement web/js origin checks
+          return cb(null, client, redirectURI, 'http://localhost:3001');
+        }
+      }
+  
+  
+      if (redirectURI && client.redirectURIs.indexOf(redirectURI) == -1) {
+        return cb(new oauth2orize.AuthorizationError('Client not permitted to use redirect URI', 'unauthorized_client'));
+      }
+  
+      return cb(null, client, redirectURI || client.redirectURIs[0], 'http://localhost:3001');
     }); // realms.resolve
   };
 };
