afd74c4d1bf3b0cea309fd26538734b398787dc7
Jared Hanson <jaredhanson@gmail.com>
2018-05-15 16:51:39 -0700

    Add tests.

--- test/http/handlers/authorize/processtransaction.test.js
+++ test/http/handlers/authorize/processtransaction.test.js
@@ -3,6 +3,7 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var factory = require('../../../../app/http/handlers/authorize/processtransaction');
+var EventEmitter = require('events').EventEmitter;
 
 
 describe('http/handlers/authorize/processtransaction', function() {
@@ -13,17 +14,35 @@ describe('http/handlers/authorize/processtransaction', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/oauth2/http/authorize/processTransactionFunc');
+    expect(factory['@singleton']).to.be.undefined;
   });
   
   describe('processTransaction', function() {
+    var aaa = {
+      request: function(){}
+    }
     
-    describe.skip('that requires login', function() {
-      function service(req, txn) {
-        txn.prompt('login');
-      }
-      serviceSpy = sinon.spy(service);
     
+    describe('that requires login', function() {
       var allow, info;
+      
+      before(function() {
+        var dreq = new EventEmitter();
+        var dec = new EventEmitter();
+        dreq.send = function() {
+          process.nextTick(function() {
+            dec.emit('prompt', 'login');
+            dec.emit('end');
+          });
+        };
+        
+        sinon.stub(aaa, 'request').returns(dreq).yields(dec);
+      });
+      
+      after(function() {
+        aaa.request.restore();
+      });
+      
       before(function(done) {
         var client = {
           id: 's6BhdRkqt3',
@@ -37,23 +56,24 @@ describe('http/handlers/authorize/processtransaction', function() {
           clientID: 's6BhdRkqt3'
         };
       
-        var processTransaction = factory(serviceSpy);
-        processTransaction(client, undefined, areq.scope, areq.type, areq, undefined, function(e, a, i) {
-          if (e) { return done(e); }
+        var processTransaction = factory(aaa);
+        processTransaction(client, undefined, areq.scope, areq.type, areq, undefined, function(err, a, i) {
+          if (err) { return done(err); }
           allow = a;
           info = i;
           done();
         });
       });
       
-      it('should now allow authorization', function() {
+      it('should now allow', function() {
         expect(allow).to.equal(false);
       });
       
       it('should prompt', function() {
         expect(info).to.deep.equal({ prompt: 'login' });
       });
-    });
-  });
+    }); // that requires login
+    
+  }); // processTransaction
   
 });
