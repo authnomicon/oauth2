726f6345400705dd8bddd1992ba09db07d643132
Jared Hanson <jaredhanson@gmail.com>
2018-05-16 14:15:37 -0700

    Clarify tests.

--- test/http/handlers/authorize.test.js
+++ test/http/handlers/authorize.test.js
@@ -120,56 +120,8 @@ describe('http/handlers/authorize', function() {
         expect(response.statusCode).to.equal(200);
         expect(view).to.equal('consent');
       });
-    });
+    }); // default behavior
     
-  });
-  
-  /*
-  describe('creating handler', function() {
-    var server = {
-      authorization: function(){},
-      authorizationErrorHandler: function(){}
-    };
-    var validateClient = function(){};
-    var processTransaction = function(){};
-    var completeTransaction = function(){};
-    
-    var authenticate = function(){};
-    var authorization = function(){};
-    var prompt = function(){};
-    var errorLogging = function(){};
-    var errorHandler = function(){};
-    
-    
-    var authenticateStub = sinon.stub().returns(authenticate);
-    var authorizationStub = sinon.stub(server, 'authorization').returns(authorization);
-    var promptStub = sinon.stub().returns(prompt);
-    var errorLoggingStub = sinon.stub().returns(errorLogging);
-    var errorHandlerStub = sinon.stub(server, 'authorizationErrorHandler').returns(errorHandler);
-    
-    var handler = factory(server, validateClient, processTransaction, completeTransaction, promptStub, authenticateStub, errorLoggingStub);
-    
-    it('should return handler', function() {
-      expect(handler).to.be.an('array');
-      expect(handler[0]).to.equal(authenticate);
-      expect(handler[1]).to.equal(authorization);
-      expect(handler[2]).to.equal(prompt);
-      expect(handler[3]).to.equal(errorLogging);
-      expect(handler[4]).to.equal(errorHandler);
-    });
-    
-    it('should apply authenticate', function() {
-      expect(authenticateStub).to.have.been.calledWithExactly([ 'session', 'anonymous' ]);
-    });
-    
-    it('should apply authorization', function() {
-      expect(authorizationStub).to.have.been.calledWithExactly(validateClient, processTransaction, completeTransaction);
-    });
-    
-    it('should apply error handler', function() {
-      expect(errorHandlerStub).to.have.been.calledOnce;
-    });
-  });
-  */
+  }); // handler
   
 });
