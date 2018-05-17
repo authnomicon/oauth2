f514df50c6c3b0a917a3c719aef5ce48cf7c9740
Jared Hanson <jaredhanson@gmail.com>
2018-05-17 08:55:25 -0700

    Update interface.

--- app/ext/modes/webmessage.js
+++ app/ext/modes/webmessage.js
@@ -2,5 +2,5 @@ exports = module.exports = function() {
   return require('oauth2orize-wmrm');
 };
 
-exports['@implements'] = 'http://schemas.authnomicon.org/js/oauth2/responseMode';
+exports['@implements'] = 'http://schemas.authnomicon.org/js/http/oauth2/ResponseMode';
 exports['@mode'] = 'web_message';
