e49453f58e42d36e3f4aeaf85602206af75f99d8
Jared Hanson <jaredhanson@gmail.com>
2018-05-17 11:18:47 -0700

    Update tests.

--- test/http/grant/code/issue/token.test.js
+++ test/http/grant/code/issue/token.test.js
@@ -5,10 +5,15 @@ var sinon = require('sinon');
 var factory = require('../../../../../app/http/grant/code/issue/token');
 
 
-describe('code/issue/token', function() {
+describe('http/grant/code/issue/token', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.be.undefined;
+    expect(factory['@singleton']).to.be.undefined;
   });
   
   /*
