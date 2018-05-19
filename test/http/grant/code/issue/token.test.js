522a3e26c49361f9680f1dea91e8acec37d18c9f
Jared Hanson <jaredhanson@gmail.com>
2018-05-19 08:37:53 -0700

    Add tests.

--- test/http/grant/code/issue/token.test.js
+++ test/http/grant/code/issue/token.test.js
@@ -81,6 +81,37 @@ describe('http/grant/code/issue/token', function() {
         expect(tokens.decode.args[0][1]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
       });
       
+      it('should get resource from directory services', function() {
+        expect(ds.get.callCount).to.equal(1);
+        expect(ds.get.args[0][0]).to.equal('112210f47de98100');
+        expect(ds.get.args[0][1]).to.equal('resources');
+      });
+      
+      it('should issue access', function() {
+        expect(sts.issue.callCount).to.equal(1);
+        expect(sts.issue.args[0][0]).to.deep.equal({
+          user: {
+            id: '1',
+            displayName: 'John Doe'
+          },
+          client: {
+            id: 's6BhdRkqt3',
+            name: 'Example Client'
+          },
+          permissions: [ {
+            resource: {
+              id: '112210f47de98100',
+              identifier: 'https://api.example.com/',
+              name: 'Example API'
+            },
+            scope: [ 'read:foo', 'write:foo', 'read:bar' ]
+          } ],
+          audience: [ {
+            id: 'http://localhost/userinfo'
+          } ]
+        });
+      });
+      
       it('should yield access token', function() {
         expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
       });
