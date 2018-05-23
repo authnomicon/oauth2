4006f1b0bf4f8c4ef05b6876eeddd218e322a043
Jared Hanson <jaredhanson@gmail.com>
2018-05-23 10:25:00 -0700

    Add tests.

--- test/sts/issue.test.js
+++ test/sts/issue.test.js
@@ -61,6 +61,15 @@ describe('sts/issue', function() {
       
       it('should negotiate token type', function() {
         expect(sts.issue.callCount).to.equal(1);
+        /*
+        expect(sts.issue.args[0][0]).to.deep.equal({
+          user: {
+            id: '1',
+            displayName: 'John Doe'
+          },
+          scope: [ 'beep', 'boop' ]
+        });
+        */
         expect(sts.issue.args[0][1]).to.deep.equal([
           { id: '112210f47de98100',
             identifier: 'https://api.example.com/',
@@ -71,43 +80,6 @@ describe('sts/issue', function() {
           name: 'Example Client'
         });
       });
-      
-      /*
-      it('should negotiate token format', function() {
-        expect(negotiateFormatStub.callCount).to.equal(1);
-        expect(negotiateFormatStub.args[0][0]).to.deep.equal([
-          { id: '112210f47de98100',
-            identifier: 'https://api.example.com/',
-            name: 'Example API' }
-        ]);
-      });
-      */
-      
-      /*
-      it('should encode token', function() {
-        expect(tokens.encode.callCount).to.equal(1);
-        expect(tokens.encode.args[0][0]).to.equal('access');
-        expect(tokens.encode.args[0][1]).to.deep.equal({
-          user: {
-            id: '1',
-            displayName: 'John Doe'
-          },
-          client: {
-            id: 's6BhdRkqt3',
-            name: 'Example Client'
-          },
-          permissions: [ {
-            resource: {
-              id: '112210f47de98100',
-              identifier: 'https://api.example.com/',
-              name: 'Example API'
-            },
-            scope: [ 'read:foo', 'write:foo', 'read:bar' ]
-          } ],
-          redirectURI: 'https://client.example.com/cb'
-        });
-      });
-      */
     
       it('should yield token', function() {
         expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
