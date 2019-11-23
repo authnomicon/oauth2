f04e1e9dcd69c03ea2e82184c2764397fc008c43
Jared Hanson <jaredhanson@gmail.com>
2019-11-23 11:00:10 -0800

    Move consent yield to attic.

--- app/http/ceremony/authorize.js
+++ app/http/ceremony/authorize.js
@@ -1,4 +1,14 @@
-exports = module.exports = function(prompt, resume, exit, yieldLogin, yieldConsent) {
+exports = module.exports = function(prompt, resume, exit, yieldLogin /*, yieldConsent*/) {
+  
+  /*
+  return {
+    resume: prompt,
+    exit: exit,
+    yields: {
+      'login': yieldLogin
+    }
+  };
+  */
   
   return {
     prompt: prompt,
@@ -10,12 +20,12 @@ exports = module.exports = function(prompt, resume, exit, yieldLogin, yieldConse
   };
 };
 
-exports['@implements'] = 'http://i.bixbyjs.org/http/ceremony/Prompt';
-exports['@name'] = 'oauth2/authorize';
+exports['@implements'] = 'http://i.bixbyjs.org/http/ceremony/Prompt2';
+exports['@name'] = '/oauth2/authorize';
 exports['@require'] = [
   './authorize/prompt',
   './authorize/resume',
   './authorize/exit',
   './authorize/yield/login',
-  './authorize/yield/consent'
+  //'./authorize/yield/consent'
 ];
