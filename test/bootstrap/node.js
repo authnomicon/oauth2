de8d7e02ffd4b3b2319c0b10e38380bddbf9334b
Jared Hanson <jaredhanson@gmail.com>
2020-01-02 13:09:59 -0800

    Update devDependencies.

--- test/bootstrap/node.js
+++ test/bootstrap/node.js
@@ -1,5 +1,5 @@
 var chai = require('chai');
 
+chai.use(require('sinon-chai'));
 chai.use(require('chai-date-proximity'));
 chai.use(require('chai-express-handler'));
-chai.use(require('sinon-chai'));
