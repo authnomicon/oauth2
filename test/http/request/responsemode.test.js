b3cab402a62f84766bf6834d07f45a1aa2afce90
Jared Hanson <jaredhanson@gmail.com>
2020-01-03 09:00:02 -0800

    Add tests.

--- test/http/request/responsemode.test.js
+++ test/http/request/responsemode.test.js
@@ -6,7 +6,7 @@ var sinon = require('sinon');
 var factory = require('../../../app/http/request/responsemode');
 
 
-describe('http/handlers/authorize/params/responsemode', function() {
+describe('http/request/responsemode', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
@@ -16,6 +16,18 @@ describe('http/handlers/authorize/params/responsemode', function() {
     expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/http/oauth2/RequestParameters');
     expect(factory['@name']).to.equal('response_mode');
     expect(factory['@singleton']).to.be.undefined;
+  });
+  
+  describe('creating extensions', function() {
+    var extensionsSpy = sinon.stub();
+    
+    var factory = $require('../../../app/http/request/responsemode',
+      { 'oauth2orize-response-mode': { extensions: extensionsSpy } });
+    var extensions = factory();
+    
+    it('should create extensions', function() {
+      expect(extensionsSpy).to.have.been.calledOnce;
+    });
   });
   
 });
--- test/http/request/webmessage.test.js
+++ test/http/request/webmessage.test.js
@@ -6,7 +6,7 @@ var sinon = require('sinon');
 var factory = require('../../../app/http/request/webmessage');
 
 
-describe('http/handlers/authorize/params/webmessage', function() {
+describe('http/request/webmessage', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
@@ -16,6 +16,18 @@ describe('http/handlers/authorize/params/webmessage', function() {
     expect(factory['@implements']).to.equal('http://schemas.authnomicon.org/js/http/oauth2/RequestParameters');
     expect(factory['@name']).to.equal('web_message_uri');
     expect(factory['@singleton']).to.be.undefined;
+  });
+  
+  describe('creating extensions', function() {
+    var extensionsSpy = sinon.stub();
+    
+    var factory = $require('../../../app/http/request/webmessage',
+      { 'oauth2orize-wmrm': { extensions: extensionsSpy } });
+    var extensions = factory();
+    
+    it('should create extensions', function() {
+      expect(extensionsSpy).to.have.been.calledOnce;
+    });
   });
   
 });
