17edaaa3d17b4c1c3151fbd3e1f447065450f009
Jared Hanson <jaredhanson@gmail.com>
2020-01-03 13:48:36 -0800

    Move response modes.

--- test/authorize/http/response/modes/formpost.test.js
+++ test/authorize/http/response/modes/formpost.test.js
@@ -2,10 +2,10 @@
 
 var $require = require('proxyquire');
 var expect = require('chai').expect;
-var factory = require('../../../../app/http/response/modes/webmessage');
+var factory = require('../../../../../app/authorize/http/response/modes/formpost');
 
 
-describe('http/handlers/authorize/modes/webmessage', function() {
+describe('http/response/modes/formpost', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
@@ -13,7 +13,7 @@ describe('http/handlers/authorize/modes/webmessage', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/oauth2/authorize/http/ResponseMode');
-    expect(factory['@mode']).to.equal('web_message');
+    expect(factory['@mode']).to.equal('form_post');
     expect(factory['@singleton']).to.be.undefined;
   });
   
--- test/authorize/http/response/modes/webmessage.test.js
+++ test/authorize/http/response/modes/webmessage.test.js
@@ -2,10 +2,10 @@
 
 var $require = require('proxyquire');
 var expect = require('chai').expect;
-var factory = require('../../../../app/http/response/modes/cookie');
+var factory = require('../../../../../app/authorize/http/response/modes/webmessage');
 
 
-describe('http/response/modes/cookie', function() {
+describe('http/handlers/authorize/modes/webmessage', function() {
   
   it('should export factory function', function() {
     expect(factory).to.be.a('function');
@@ -13,7 +13,7 @@ describe('http/response/modes/cookie', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/oauth2/authorize/http/ResponseMode');
-    expect(factory['@mode']).to.equal('cookie');
+    expect(factory['@mode']).to.equal('web_message');
     expect(factory['@singleton']).to.be.undefined;
   });
   
