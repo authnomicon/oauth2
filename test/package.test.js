0eabb8786bb01cb96964812cb602d19feb01104a
Jared Hanson <jaredhanson@gmail.com>
2020-01-04 12:03:24 -0800

    Fix test.

--- test/package.test.js
+++ test/package.test.js
@@ -19,7 +19,7 @@ describe('@authnomicon/oauth2', function() {
       
       expect(json.assembly.components).to.have.length(20);
       expect(json.assembly.components).to.include('authorize/http/service');
-      expect(json.assembly.components).to.include('http/token');
+      expect(json.assembly.components).to.include('token/http/service');
     });
   });
   
