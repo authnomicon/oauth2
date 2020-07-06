ff398cee36a6df71074bc324f002706772e41dfd
Jared Hanson <jaredhanson@gmail.com>
2020-07-06 11:44:25 -0700

    Add test.

--- test/token/http/auth/post.test.js
+++ test/token/http/auth/post.test.js
@@ -242,4 +242,61 @@ describe('password/http/scheme', function() {
     
   }); // encountering error while verifying client identifier and secret
   
+  describe('encountering error while querying directory', function() {
+    var secrets = new Object();
+    secrets.verify = sinon.stub().yieldsAsync(null, true);
+    var clients = new Object();
+    clients.find = sinon.stub().yieldsAsync(new Error('something went wrong'));
+    
+    var StrategySpy = sinon.spy(Strategy);
+    
+    var factory = $require('../../../../app/token/http/auth/post',
+      { 'passport-oauth2-client-password': { Strategy: StrategySpy } });
+    var strategy = factory(secrets, clients);
+    
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+    
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+    
+    describe('verify', function() {
+      var error, client, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw', function(e, c, i) {
+          error = e;
+          client = c;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify credentials', function() {
+        expect(secrets.verify).to.calledOnceWith('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw');
+      });
+      
+      it('should query directory', function() {
+        expect(clients.find).to.have.been.calledOnceWith('s6BhdRkqt3');
+      });
+      
+      it('should yield error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('something went wrong');
+      });
+    
+      it('should not yield client', function() {
+        expect(client).to.be.undefined;
+      });
+    
+      it('should not yield info', function() {
+        expect(info).to.be.undefined;
+      });
+    }); // verify
+    
+  }); // encountering error while querying directory
+  
 });
