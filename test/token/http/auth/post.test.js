831631ce8b2029fa8167fe06f7e3a6a5facd20c2
Jared Hanson <jaredhanson@gmail.com>
2020-07-06 11:37:17 -0700

    Add tests.

--- test/token/http/auth/post.test.js
+++ test/token/http/auth/post.test.js
@@ -133,4 +133,113 @@ describe('password/http/scheme', function() {
     
   }); // verifying client identifier and secret using credential service with directory capability
   
+  describe('not verifying client identifier and secret', function() {
+    var secrets = new Object();
+    secrets.verify = sinon.stub().yieldsAsync(null, false);
+    var clients = new Object();
+    clients.find = sinon.stub().yieldsAsync(null, { id: 's6BhdRkqt3', displayName: 'My Example Client' });
+    
+    var StrategySpy = sinon.spy(Strategy);
+    
+    var factory = $require('../../../../app/token/http/auth/post',
+      { 'passport-oauth2-client-password': { Strategy: StrategySpy } });
+    var strategy = factory(secrets, clients);
+    
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+    
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+    
+    describe('verify', function() {
+      var client, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw', function(e, c, i) {
+          if (e) { return done(e); }
+          client = c;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify credentials', function() {
+        expect(secrets.verify).to.calledOnceWith('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw');
+      });
+      
+      it('should not query directory', function() {
+        expect(clients.find).to.not.have.been.called;
+      });
+      
+      it('should not yield client', function() {
+        expect(client).to.equal(false);
+      });
+    
+      it('should not yield info', function() {
+        expect(info).to.be.undefined;
+      });
+    }); // verify
+    
+  }); // not verifying client identifier and secret
+  
+  describe('encountering error while verifying client identifier and secret', function() {
+    var secrets = new Object();
+    secrets.verify = sinon.stub().yieldsAsync(new Error('something went wrong'));
+    var clients = new Object();
+    clients.find = sinon.stub().yieldsAsync(null, { id: 's6BhdRkqt3', displayName: 'My Example Client' });
+    
+    var StrategySpy = sinon.spy(Strategy);
+    
+    var factory = $require('../../../../app/token/http/auth/post',
+      { 'passport-oauth2-client-password': { Strategy: StrategySpy } });
+    var strategy = factory(secrets, clients);
+    
+    it('should construct strategy', function() {
+      expect(StrategySpy).to.have.been.calledOnce;
+    });
+    
+    it('should return strategy', function() {
+      expect(strategy).to.be.an.instanceOf(Strategy);
+    });
+    
+    describe('verify', function() {
+      var error, client, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][0];
+        verify('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw', function(e, c, i) {
+          error = e;
+          client = c;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify credentials', function() {
+        expect(secrets.verify).to.calledOnceWith('s6BhdRkqt3', '7Fjfp0ZBr1KtDRbnfVdmIw');
+      });
+      
+      it('should not query directory', function() {
+        expect(clients.find).to.not.have.been.called;
+      });
+      
+      it('should yield error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('something went wrong');
+      });
+    
+      it('should not yield client', function() {
+        expect(client).to.be.undefined;
+      });
+    
+      it('should not yield info', function() {
+        expect(info).to.be.undefined;
+      });
+    }); // verify
+    
+  }); // encountering error while verifying client identifier and secret
+  
 });
