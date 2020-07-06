767fbbee63b42bdb503cd660a2fc361a8a5954e1
Jared Hanson <jaredhanson@gmail.com>
2020-07-06 12:35:39 -0700

    Add tests.

---
+++ app/token/http/auth/basic.js
@@ -0,0 +1,30 @@
+var internals = require('../../../../lib/auth/internals');
+
+
+exports = module.exports = function(secrets, clients) {
+  var Strategy = require('passport-http').BasicStrategy;
+  
+  return new Strategy(function(clientID, secret, cb) {
+    
+    secrets.verify(clientID, secret, function(err, client, info) {
+      if (err) { return cb(err); }
+      if (!client) { return cb(null, false); }
+      
+      info = info || {};
+      info.methods = [ 'password' ];
+      
+      if (typeof client == 'object') { return cb(null, client, info); }
+      clients.find(clientID, function(err, client) {
+        if (err) { return cb(err); }
+        return cb(null, client, info);
+      });
+    });
+  });
+};
+
+exports['@implements'] = 'http://i.bixbyjs.org/http/auth/Scheme';
+exports['@scheme'] = 'client_secret_basic';
+exports['@require'] = [
+  'http://i.authnomicon.org/oauth2/credentials/ClientSecretService',
+  'http://i.authnomicon.org/oauth2/ClientDirectory'
+];
