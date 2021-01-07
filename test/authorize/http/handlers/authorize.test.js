ebea3f50097189ec550cc040db19a2c6c44e4dd7
Jared Hanson <jaredhanson@gmail.com>
2021-01-06 20:41:48 -0800

    Clarify tests.

--- test/authorize/http/handlers/authorize.test.js
+++ test/authorize/http/handlers/authorize.test.js
@@ -491,11 +491,27 @@ describe('authorize/http/handlers/authorize', function() {
         ]
       });
       
+      function authenticate(idp, options) {
+        return function(req, res, next) {
+          req.user = { id: '248289761001', displayName: 'Jane Doe' };
+          next();
+        };
+      }
+      
+      function state() {
+        return function(req, res, next) {
+          next();
+        };
+      }
+      
+      var authenticateSpy = sinon.spy(authenticate);
+      var stateSpy = sinon.spy(state);
+      
       
       var error, request, response;
       
       before(function(done) {
-        var handler = factory(processRequest, clients, { authorization: authorization }, authenticate, ceremony);
+        var handler = factory(processRequest, clients, { authorization: authorization }, authenticateSpy, stateSpy);
         
         chai.express.handler(handler)
           .req(function(req) {
@@ -515,10 +531,9 @@ describe('authorize/http/handlers/authorize', function() {
           .dispatch();
       });
       
-      it('should authenticate', function() {
-        expect(request.authInfo).to.deep.equal({
-          mechanisms: ['session', 'anonymous']
-        });
+      it('should setup middleware', function() {
+        expect(stateSpy).to.be.calledOnceWith({ external: true, continue: '/oauth2/authorize/continue' });
+        expect(authenticateSpy).to.be.calledOnceWith([ 'session', 'anonymous' ]);
       });
       
       it('should query directory', function() {
@@ -548,11 +563,27 @@ describe('authorize/http/handlers/authorize', function() {
         ]
       });
       
+      function authenticate(idp, options) {
+        return function(req, res, next) {
+          req.user = { id: '248289761001', displayName: 'Jane Doe' };
+          next();
+        };
+      }
+      
+      function state() {
+        return function(req, res, next) {
+          next();
+        };
+      }
+      
+      var authenticateSpy = sinon.spy(authenticate);
+      var stateSpy = sinon.spy(state);
+      
       
       var error, request, response;
       
       before(function(done) {
-        var handler = factory(processRequest, clients, { authorization: authorization }, authenticate, ceremony);
+        var handler = factory(processRequest, clients, { authorization: authorization }, authenticateSpy, stateSpy);
         
         chai.express.handler(handler)
           .req(function(req) {
@@ -571,10 +602,9 @@ describe('authorize/http/handlers/authorize', function() {
           .dispatch();
       });
       
-      it('should authenticate', function() {
-        expect(request.authInfo).to.deep.equal({
-          mechanisms: ['session', 'anonymous']
-        });
+      it('should setup middleware', function() {
+        expect(stateSpy).to.be.calledOnceWith({ external: true, continue: '/oauth2/authorize/continue' });
+        expect(authenticateSpy).to.be.calledOnceWith([ 'session', 'anonymous' ]);
       });
       
       it('should query directory', function() {
