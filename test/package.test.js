6c76b139a5701cc9cb887c493f496a21b1c8b581
Jared Hanson <jaredhanson@gmail.com>
2021-01-09 17:44:07 -0800

    Expose renamed components correctly.

--- test/package.test.js
+++ test/package.test.js
@@ -19,7 +19,7 @@ describe('@authnomicon/oauth2', function() {
       
       expect(json.assembly.components).to.have.length(22);
       expect(json.assembly.components).to.include('http/authorize/service');
-      expect(json.assembly.components).to.include('token/http/service');
+      expect(json.assembly.components).to.include('http/token/service');
     });
   });
   
