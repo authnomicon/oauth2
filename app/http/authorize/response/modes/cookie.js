dce9df8be72ddb8bda65d70194c74469cae1a437
Jared Hanson <jaredhanson@gmail.com>
2021-10-03 07:29:22 -0700

    Remove cookie response mode and update response mode dependencies.

--- app/http/authorize/response/modes/cookie.js
+++
@@ -1,6 +0,0 @@
-exports = module.exports = function() {
-  return require('oauth2orize-cookierm');
-};
-
-exports['@implements'] = 'http://i.authnomicon.org/oauth2/authorize/http/ResponseMode';
-exports['@mode'] = 'cookie';
