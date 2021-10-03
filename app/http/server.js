3399d3abb8489a23dd84c1ae02fb577bdba1a863
Jared Hanson <jaredhanson@gmail.com>
2021-10-03 07:54:19 -0700

    More robust loading of request parameter plugins.

--- app/http/server.js
+++ app/http/server.js
@@ -9,18 +9,33 @@ exports = module.exports = function(container, store, logger) {
   return Promise.resolve(server)
     .then(function(server) {
       // Register request parameter extensions with the OAuth 2.0 server.
-      var paramDecls = container.components('http://i.authnomicon.org/oauth2/http/request/Parameters');
-    
-      return Promise.all(paramDecls.map(function(spec) { return container.create(spec.id); } ))
-        .then(function(plugins) {
-          plugins.forEach(function(plugin, i) {
-            server.grant(plugin);
-            logger.info('Loaded OAuth 2.0 request parameters: ' + paramDecls[i].id);
-          });
-        })
-        .then(function() {
-          return server;
-        });
+      return new Promise(function(resolve, reject) {
+        var components = container.components('http://i.authnomicon.org/oauth2/authorization/http/RequestParameters');
+        
+        (function iter(i) {
+          var component = components[i];
+          if (!component) {
+            return resolve(server);
+          }
+          
+          component.create()
+            .then(function(requestParams) {
+              logger.info('Loaded OAuth 2.0 authorization request parameters: ' + component.id);
+              server.grant(requestParams);
+              iter(i + 1);
+            }, function(err) {
+              // TODO: Print the package name in the error, so it can be found
+              // TODO: Make the error have the stack of dependencies.
+              if (err.code == 'IMPLEMENTATION_NOT_FOUND') {
+                logger.notice(err.message + ' while loading component ' + component.id);
+                // TODO: Mount an implementation not found indicator in development
+                return iter(i + 1);
+              }
+              
+              reject(err);
+            });
+        })(0);
+      });
     })
     .then(function(server) {
       // Register response types with the OAuth 2.0 server.
@@ -38,7 +53,7 @@ exports = module.exports = function(container, store, logger) {
           
           component.create()
             .then(function(responseType) {
-              logger.info('Loaded OAuth 2.0 response type: ' + (type || responseType.name));
+              logger.info('Loaded OAuth 2.0 authorization response type: ' + (type || responseType.name));
               server.grant(type || responseType.name, responseType);
               iter(i + 1);
             }, function(err) {
