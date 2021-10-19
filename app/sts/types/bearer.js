8f4a1e2ca2fab0fa9baa683725e8508974654b91
Jared Hanson <jaredhanson@gmail.com>
2021-10-19 06:57:48 -0700

    Remove sts-related files. Moved to @authnomicon/sts.

--- app/sts/types/bearer.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function() {
-  
-  return function bearer(params) {
-    if (Object.keys(params).length !== 0) { return; }
-    return { token_type: 'Bearer' };
-  };
-};
