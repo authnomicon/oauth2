3d83221c22d6592db9f3e52e636be121245e3611
Jared Hanson <jaredhanson@gmail.com>
2021-10-19 10:36:54 -0700

    * Remove attic. *

--- attic/clients/directory/federated/configuration.js
+++
@@ -1,6 +0,0 @@
-exports = module.exports = function(WWW) {
-  return require('oauth2-client-configuration')({ request: WWW });
-};
-
-exports['@implements'] = 'http://schemas.modulate.io/js/aaa/oauth2/federation/clientMetadataFunc';
-exports['@require'] = [ 'http://i.bixbyjs.org/www/Client' ];
