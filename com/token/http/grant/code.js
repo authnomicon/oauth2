7386974fd4c05dcc0dbae543f8493ae2e2176d86
Jared Hanson <jaredhanson@gmail.com>
2021-11-19 08:59:17 -0800

    Indentation.

--- com/token/http/grant/code.js
+++ com/token/http/grant/code.js
@@ -32,79 +32,79 @@ exports = module.exports = function(ats, acs, logger, C) {
     })
     .then(function(extensions) {
       
-  return oauth2orize.exchange.code(function(client, code, redirectURI, body, authInfo, cb) {
-    // TODO: Pass self trust store to token verify, using list of issuers like `ca` to Node's http
-    // module
+      return oauth2orize.exchange.code(function(client, code, redirectURI, body, authInfo, cb) {
+        // TODO: Pass self trust store to token verify, using list of issuers like `ca` to Node's http
+        // module
 
-    acs.verify(code, function(err, claims) {
-      if (err) { return cb(err); }
-  
-      var conf, i, len;
-    
-      // Verify that the authorization code was issued to the client that is
-      // attempting to exchange it for an access token.
-      if (client.id !== claims.client.id) {
-        return cb(null, false);
-      }
-  
-      // TODO: Simplify this so claims just contains redirect_uri directly
-      if (claims.confirmation) {
-    
-        for (i = 0, len = claims.confirmation.length; i < len; ++i) {
-          conf = claims.confirmation[i];
-      
-          switch (conf.method) {
-          case 'redirect-uri':
-            // Verify that the redirect URI matches the value sent in the
-            // initial authorization request.
-            // 
-            // Refer to Section 4.1.3 of RFC 6749 for further details.
-            if (redirectURI !== conf.uri) {
-              return cb(new oauth2orize.TokenError('Mismatched redirect URI', 'invalid_grant'));
-            }
-            break;
-        
-          default:
-            return cb(new Error('Unsupported code confirmation method: ' + conf.name));
-          }
-        }
-      }
-  
-      if (err) { return cb(err); }
-      
-      var params = {};
-  
-      var msg = {};
-      msg.user = claims.user;
-      msg.client = client;
-      /*
-      msg.permissions = [
-        { resource: resource, scope: claims.permissions[0].scope }
-      ];
-      */
-      //var audience = [ resource ];
-      var audience = [];
-  
-      ats.issue(msg, function(err, token) {
-        if (err) { return cb(err); }
-        var txn = {};
-        var i = 0;
-        
-        (function iter(err, exparams) {
+        acs.verify(code, function(err, claims) {
           if (err) { return cb(err); }
-          
-          if (exparams) { merge(params, exparams); }
-          
-          var extension = extensions[i++];
-          if (!extension) {
-            return cb(null, token, null, params);
+  
+          var conf, i, len;
+    
+          // Verify that the authorization code was issued to the client that is
+          // attempting to exchange it for an access token.
+          if (client.id !== claims.client.id) {
+            return cb(null, false);
           }
+  
+          // TODO: Simplify this so claims just contains redirect_uri directly
+          if (claims.confirmation) {
+    
+            for (i = 0, len = claims.confirmation.length; i < len; ++i) {
+              conf = claims.confirmation[i];
+      
+              switch (conf.method) {
+              case 'redirect-uri':
+                // Verify that the redirect URI matches the value sent in the
+                // initial authorization request.
+                // 
+                // Refer to Section 4.1.3 of RFC 6749 for further details.
+                if (redirectURI !== conf.uri) {
+                  return cb(new oauth2orize.TokenError('Mismatched redirect URI', 'invalid_grant'));
+                }
+                break;
+        
+              default:
+                return cb(new Error('Unsupported code confirmation method: ' + conf.name));
+              }
+            }
+          }
+  
+          if (err) { return cb(err); }
+      
+          var params = {};
+  
+          var msg = {};
+          msg.user = claims.user;
+          msg.client = client;
+          /*
+          msg.permissions = [
+            { resource: resource, scope: claims.permissions[0].scope }
+          ];
+          */
+          //var audience = [ resource ];
+          var audience = [];
+  
+          ats.issue(msg, function(err, token) {
+            if (err) { return cb(err); }
+            var txn = {};
+            var i = 0;
+        
+            (function iter(err, exparams) {
+              if (err) { return cb(err); }
           
-          extension(txn, iter)
-        })();
+              if (exparams) { merge(params, exparams); }
+          
+              var extension = extensions[i++];
+              if (!extension) {
+                return cb(null, token, null, params);
+              }
+          
+              extension(txn, iter)
+            })();
+          });
+        });
       });
-    });
-  });
       
     });
 }
