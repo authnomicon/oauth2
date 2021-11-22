7b1a60d3065f74052894ee59cdfa9f62d0bd9a7c
Jared Hanson <jaredhanson@gmail.com>
2021-11-22 07:35:42 -0800

    Clean up tests.

--- test/token/http/grant/code.test.js
+++ test/token/http/grant/code.test.js
@@ -54,16 +54,16 @@ describe('http/token/grant/code', function() {
       .catch(done);
   });
 
-  describe('creating exchange', function() {
+  describe('issue', function() {
     var container = new Object();
     container.components = sinon.stub()
     container.components.withArgs('http://i.authnomicon.org/oauth2/token/http/ResponseParameters').returns([]);
-    
     var logger = {
-      notice: function(){},
+      error: function(){},
       warning: function(){},
+      notice: function(){},
       info: function(){}
-    }
+    };
     
     var acs = new Object();
     acs.verify = sinon.stub().yieldsAsync(null, {
@@ -81,74 +81,55 @@ describe('http/token/grant/code', function() {
         scope: [ 'profile', 'email' ]
       }
     });
-    var sts = new Object();
-    sts.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
+    var ats = new Object();
+    ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
     
-    var codeSpy = sinon.stub();
-    var issue = function(){};
-    
-    var factory = $require('../../../../com/token/http/grant/code',
-      { 'oauth2orize': { exchange: { code: codeSpy } } });
-    
-    var exchange;
-    before(function(done) {
-      var promise = factory(sts, acs, logger, container);
-      promise.then(function(e) {
-        exchange = e;
-        done();
-      })
-      .catch(function(error) {
-        console.log(error)
-      });
-    });
-    
-    
-    it('should create exchange', function() {
-      expect(codeSpy.callCount).to.equal(1);
-      expect(codeSpy.args[0][0]).to.be.a('function');
-    });
-    
-    describe('issue', function() {
+    it('should do something', function(done) {
       var token;
       
-      before(function(done) {
-        var client = {
-          id: 's6BhdRkqt3',
-          name: 'Example Client',
-          redirectURIs: [ 'https://client.example.com/cb' ]
-        };
+      
+      var codeSpy = sinon.stub();
+      var issue = function(){};
+    
+      var factory = $require('../../../../com/token/http/grant/code',
+        { 'oauth2orize': { exchange: { code: codeSpy } } });
+      
+      var client = {
+        id: 's6BhdRkqt3',
+        name: 'Example Client',
+        redirectURIs: [ 'https://client.example.com/cb' ]
+      };
+      
+      
+      factory(ats, acs, logger, container)
+        .then(function(exchange) {
+          var issue = codeSpy.args[0][0];
+          issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.com/cb', {}, {}, function(err, t) {
+            if (err) { return done(err); }
+            token = t;
         
-        var issue = codeSpy.args[0][0];
-        issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.com/cb', {}, {}, function(err, t) {
-          if (err) { return done(err); }
-          token = t;
-          done();
-        });
-      });
-      
-      it('should verify authorization code', function() {
-        expect(acs.verify.callCount).to.equal(1);
-        expect(acs.verify.args[0][0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
-      });
-      
-      it('should issue access token', function() {
-        expect(sts.issue.callCount).to.equal(1);
-        expect(sts.issue.args[0][0]).to.deep.equal({
-          client: {
-            id: 's6BhdRkqt3',
-            name: 'Example Client',
-            redirectURIs: [ 'https://client.example.com/cb' ]
-          },
-          user: {
-            id: '248289761001',
-            displayName: 'Jane Doe'
-          }
-        });
-      });
-      
-      it('should yield access token', function() {
-        expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
-      });
+            expect(acs.verify.callCount).to.equal(1);
+            expect(acs.verify.args[0][0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
+        
+            expect(ats.issue.callCount).to.equal(1);
+            expect(ats.issue.args[0][0]).to.deep.equal({
+              client: {
+                id: 's6BhdRkqt3',
+                name: 'Example Client',
+                redirectURIs: [ 'https://client.example.com/cb' ]
+              },
+              user: {
+                id: '248289761001',
+                displayName: 'Jane Doe'
+              }
+            });
+        
+            expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
+        
+            done();
+          });
+        })
+        .catch(done);
     }); // issue
     
   }); // creating exchange
