da0e880df7525cd258d5edd866a90e0d70081814
Jared Hanson <jaredhanson@gmail.com>
2021-11-23 14:54:13 -0800

    Add tests.

--- test/token/http/grant/code.test.js
+++ test/token/http/grant/code.test.js
@@ -193,6 +193,67 @@ describe('http/token/grant/code', function() {
         .catch(done);
     }); // should not issue access token when authorization code not issued to client
     
+    it('should issue access token with authentication context', function(done) {
+      var codeSpy = sinon.stub();
+      var factory = $require('../../../../com/token/http/grant/code', {
+        'oauth2orize': { exchange: { code: codeSpy } }
+      });
+      
+      var acs = new Object();
+      acs.verify = sinon.stub().yieldsAsync(null, {
+        client: { id: 's6BhdRkqt3' },
+        redirectURI: 'https://client.example.org/cb',
+        user: { id: '248289761001' },
+        scope: [ 'profile', 'email' ],
+        authContext: {
+          sessionID: 'YU7uoYRVAxF34TuoAodVfw-1eA13rhqW',
+          methods: [
+            { method: 'password', timestamp: new Date('2011-07-21T20:42:49.000Z') }
+          ]
+        }
+      });
+      var ats = new Object();
+      ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
+      
+      factory(ats, acs, logger, container)
+        .then(function(exchange) {
+          var client = {
+            id: 's6BhdRkqt3',
+            name: 'Example Client',
+            redirectURIs: [ 'https://client.example.org/cb' ]
+          };
+          
+          var issue = codeSpy.getCall(0).args[0];
+          issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.org/cb', {}, {}, function(err, token) {
+            if (err) { return done(err); }
+        
+            expect(acs.verify).to.be.calledOnce;
+            expect(acs.verify.getCall(0).args[0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
+            expect(ats.issue).to.be.calledOnce;
+            expect(ats.issue.getCall(0).args[0]).to.deep.equal({
+              user: {
+                id: '248289761001'
+              },
+              client: {
+                id: 's6BhdRkqt3',
+                name: 'Example Client',
+                redirectURIs: [ 'https://client.example.org/cb' ]
+              },
+              scope: [ 'profile', 'email' ],
+              authContext: {
+                sessionID: 'YU7uoYRVAxF34TuoAodVfw-1eA13rhqW',
+                methods: [
+                  { method: 'password', timestamp: new Date('2011-07-21T20:42:49.000Z') }
+                ]
+              }
+            });
+            expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
+            done();
+          });
+        })
+        .catch(done);
+    }); // should issue access token with authentication context
+    
     it('should not issue access token when redirect URI is not identical to initial authorization request', function(done) {
       var codeSpy = sinon.stub();
       var factory = $require('../../../../com/token/http/grant/code', {
