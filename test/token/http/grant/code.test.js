1db2b1a822d4c3fdc6ec37a9202e8ca3a5d18972
Jared Hanson <jaredhanson@gmail.com>
2021-11-23 13:10:42 -0800

    Add tests.

--- test/token/http/grant/code.test.js
+++ test/token/http/grant/code.test.js
@@ -58,8 +58,7 @@ describe('http/token/grant/code', function() {
       notice: function(){},
       info: function(){}
     };
-    var ats = new Object();
-    ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
+    
     
     it('should issue access token with scope', function(done) {
       var codeSpy = sinon.stub();
@@ -74,6 +73,8 @@ describe('http/token/grant/code', function() {
         user: { id: '248289761001' },
         scope: [ 'profile', 'email' ]
       });
+      var ats = new Object();
+      ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
       
       factory(ats, acs, logger, container)
         .then(function(exchange) {
@@ -108,6 +109,45 @@ describe('http/token/grant/code', function() {
         })
         .catch(done);
     }); // should issue access token with scope
+    
+    it('should not issue access token when authorization code not issued to client', function(done) {
+      var codeSpy = sinon.stub();
+      var factory = $require('../../../../com/token/http/grant/code', {
+        'oauth2orize': { exchange: { code: codeSpy } }
+      });
+      
+      var acs = new Object();
+      acs.verify = sinon.stub().yieldsAsync(null, {
+        client: { id: 's6BhdRkqt3' },
+        redirectURI: 'https://client.example.org/cb',
+        user: { id: '248289761001' },
+        scope: [ 'profile', 'email' ]
+      });
+      var ats = new Object();
+      ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
+      
+      factory(ats, acs, logger, container)
+        .then(function(exchange) {
+          
+          var client = {
+            id: 'XXXXXXXX',
+            name: 'Example Client',
+            redirectURIs: [ 'https://client.example.org/cb' ]
+          };
+          
+          var issue = codeSpy.getCall(0).args[0];
+          issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.org/cb', {}, {}, function(err, token) {
+            if (err) { return done(err); }
+        
+            expect(acs.verify).to.be.calledOnce;
+            expect(acs.verify.getCall(0).args[0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
+            expect(ats.issue).to.not.be.called;
+            expect(token).to.be.false;
+            done();
+          });
+        })
+        .catch(done);
+    }); // should not issue access token when authorization code not issued to client
     
   }); // creating exchange
   
