5d7613dc2b094ba49d2983d306a1a956296b8926
Jared Hanson <jaredhanson@gmail.com>
2021-12-03 13:46:08 -0800

    Clean up tests.

--- test/authorize/http/handlers/continue.test.js
+++ test/authorize/http/handlers/continue.test.js
@@ -13,24 +13,68 @@ describe('authorize/http/handlers/continue', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
+  
+  function evaluate(req, res, next) {
+    res.redirect('/consent')
+  };
+  
+  var server = {
+    resume: function(immediate) {
+      return function(req, res, next) {
+        immediate(req.oauth2, function(err, allow) {
+          if (err) { return next(err); }
+          if (allow) { return res.redirect(req.oauth2.redirectURI); }
+          return next();
+        })
+      };
+    }
+  };
+  
+  function authenticate() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function state() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function session() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function parseCookies() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  it('should create handler', function() {
+    var authenticateSpy = sinon.spy(authenticate);
+    var stateSpy = sinon.spy(state);
+    var sessionSpy = sinon.spy(session);
+    var parseCookiesSpy = sinon.spy(parseCookies);
+    
+    var handler = factory(evaluate, server, authenticateSpy, stateSpy, sessionSpy, parseCookiesSpy);
+    
+    expect(parseCookiesSpy).to.be.calledOnce;
+    expect(sessionSpy).to.be.calledOnce;
+    expect(sessionSpy).to.be.calledAfter(parseCookiesSpy);
+    expect(stateSpy).to.be.calledOnce;
+    expect(stateSpy).to.be.calledWithExactly();
+    expect(stateSpy).to.be.calledAfter(sessionSpy);
+    expect(authenticateSpy).to.be.calledOnce;
+    expect(authenticateSpy).to.be.calledWithExactly([ 'session' ], { multi: true });
+    expect(authenticateSpy).to.be.calledAfter(stateSpy);
+  });
+  
+  
   describe('handler', function() {
-    
-    var server = {
-      resume: function(immediate) {
-      
-        return function(req, res, next) {
-          immediate(req.oauth2, function(err, allow) {
-            if (err) { return next(err); }
-            if (allow) { return res.redirect(req.oauth2.redirectURI); }
-            return next();
-          })
-        };
-      }
-    };
-    
-    function processRequest(req, res, next) {
-      res.redirect('/consent')
-    };
     
     
     describe('processing request', function() {
@@ -67,7 +111,7 @@ describe('authorize/http/handlers/continue', function() {
       var request, response;
       
       before(function(done) {
-        var handler = factory(processRequest, server, authenticateSpy, stateSpy, sessionSpy, parseCookies);
+        var handler = factory(evaluate, server, authenticateSpy, stateSpy, sessionSpy, parseCookies);
         
         chai.express.use(handler)
           .request(function(req, res) {
