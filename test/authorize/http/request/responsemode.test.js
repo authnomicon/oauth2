f947117a950decd6116c8a2852bdc0c23fad06f4
Jared Hanson <jaredhanson@gmail.com>
2021-12-04 07:09:38 -0800

    Clarify tests.

--- test/authorize/http/request/responsemode.test.js
+++ test/authorize/http/request/responsemode.test.js
@@ -1,32 +1,26 @@
 /* global describe, it */
 
-var $require = require('proxyquire');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../../../com/authorize/http/request/responsemode');
 
 
-describe('http/request/responsemode', function() {
-  
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
+describe('authorize/http/request/responsemode', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/oauth2/authorization/http/RequestParameters');
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe('creating extensions', function() {
+  it('should create extension', function() {
     var extensionsSpy = sinon.stub();
-    
-    var factory = $require('../../../../com/authorize/http/request/responsemode',
-      { 'oauth2orize-response-mode': { extensions: extensionsSpy } });
-    var extensions = factory();
-    
-    it('should create extensions', function() {
-      expect(extensionsSpy).to.have.been.calledOnce;
+    var factory = $require('../../../../com/authorize/http/request/responsemode', {
+      'oauth2orize-response-mode': { extensions: extensionsSpy }
     });
-  });
+    
+    var extensions = factory();
+    expect(extensionsSpy).to.have.been.calledOnce;
+  }); // should create extension
   
 });
--- test/authorize/http/request/webmessage.test.js
+++ test/authorize/http/request/webmessage.test.js
@@ -1,32 +1,26 @@
 /* global describe, it */
 
-var $require = require('proxyquire');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../../../com/authorize/http/request/webmessage');
 
 
-describe('http/request/webmessage', function() {
-  
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
+describe('authorize/http/request/webmessage', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/oauth2/authorization/http/RequestParameters');
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe('creating extensions', function() {
+  it('should create extension', function() {
     var extensionsSpy = sinon.stub();
-    
-    var factory = $require('../../../../com/authorize/http/request/webmessage',
-      { 'oauth2orize-wmrm': { extensions: extensionsSpy } });
-    var extensions = factory();
-    
-    it('should create extensions', function() {
-      expect(extensionsSpy).to.have.been.calledOnce;
+    var factory = $require('../../../../com/authorize/http/request/webmessage', {
+      'oauth2orize-wmrm': { extensions: extensionsSpy }
     });
-  });
+    
+    var extensions = factory();
+    expect(extensionsSpy).to.have.been.calledOnce;
+  }); // should create extension
   
 });
