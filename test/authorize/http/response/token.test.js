860b2886b49ed65357323dd7f3d6638321befdd1
Jared Hanson <jaredhanson@gmail.com>
2021-12-05 07:40:03 -0800

    Clean up tests.

--- test/authorize/http/response/token.test.js
+++ test/authorize/http/response/token.test.js
@@ -1,16 +1,12 @@
 /* global describe, it */
 
-var $require = require('proxyquire');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
 var sinon = require('sinon');
 var factory = require('../../../../com/authorize/http/response/token');
 
 
-describe('http/authorize/response/token', function() {
-  
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
+describe('authorize/http/response/token', function() {
   
   it('should be annotated', function() {
     expect(factory['@implements']).to.equal('http://i.authnomicon.org/oauth2/authorization/http/ResponseType');
@@ -18,46 +14,38 @@ describe('http/authorize/response/token', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe('creating grant', function() {
-    var container = {
-      components: function(){},
-      create: function(){}
-    }
-    var issue = function(){};
+  
+  var logger = {
+    emergency: function(){},
+    alert: function(){},
+    critical: function(){},
+    error: function(){},
+    warning: function(){},
+    notice: function(){},
+    info: function(){},
+    debug: function(){}
+  };
+  
+  it('should create response type without response modes', function(done) {
+    var container = new Object();
+    container.components = sinon.stub();
+    container.components.withArgs('http://i.authnomicon.org/oauth2/authorization/http/ResponseParameters').returns([]);
+    container.components.withArgs('http://i.authnomicon.org/oauth2/authorization/http/ResponseMode').returns([]);
     
+    var tokenSpy = sinon.stub();
+    var factory = $require('../../../../com/authorize/http/response/token', {
+      'oauth2orize': {
+        grant: { token: tokenSpy }
+      }
+    });
     
-    describe('without additional response modes', function() {
-      before(function() {
-        sinon.stub(container, 'components').returns([]);
-      });
-      
-      /*
-      after(function() {
-        container.components.restore();
-      });
-      */
-      
-      var tokenSpy = sinon.stub();
-      
-      var grant;
-      before(function(done) {
-        var factory = $require('../../../../com/authorize/http/response/token',
-          { 'oauth2orize': { grant: { token: tokenSpy } } });
-        
-        var promise = factory(container, issue);
-        promise.then(function(g) {
-          grant = g;
-          done();
-        });
-      });
-      
-      it('should create grant', function() {
-        expect(tokenSpy.callCount).to.equal(1);
-        expect(tokenSpy.args[0][0]).to.deep.equal({ modes: {} });
-        //expect(tokenSpy.args[0][1]).to.equal(issue);
-      });
-    }); // without additional response modes
-    
-  }); // creating grant
+    factory(container, logger)
+      .then(function(type) {
+        expect(tokenSpy).to.be.calledOnce;
+        expect(tokenSpy).to.be.calledWith({ modes: {} });
+        done();
+      })
+      .catch(done);
+  }); // should create response type without response modes
   
 });
