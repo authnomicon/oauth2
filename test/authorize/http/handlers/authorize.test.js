eeb5a8ff17954d0a9ce4d7e70223edfa39a3f1a0
Jared Hanson <jaredhanson@gmail.com>
2021-12-14 15:53:50 -0800

    Add tests.

--- test/authorize/http/handlers/authorize.test.js
+++ test/authorize/http/handlers/authorize.test.js
@@ -435,6 +435,65 @@ describe('authorize/http/handlers/authorize', function() {
         .catch(done);
     }); // should error when error is encountered while querying directory
     
+    it('should evaluate request that uses a redirect URI scheme', function(done) {
+      var scheme = {
+        verify: function(client, redirectURI) {
+          expect(client).to.deep.equal({
+            id: 's6BhdRkqt3',
+            name: 'My Example Client',
+            redirectURIs: [ 'https://client.example.com/cb' ],
+            webOrigins: [ 'https://client.example.com' ]
+          });
+          expect(redirectURI).to.equal('storagerelay://https/client.example.com?id=auth304970');
+          
+          return [ redirectURI, 'https://client.example.com' ]
+        }
+      };
+      var schemeComponent = new Object();
+      schemeComponent.create = sinon.stub().resolves(scheme);
+      schemeComponent.a = { '@scheme': 'storagerelay' };
+      
+      var container = new Object();
+      container.components = sinon.stub();
+      container.components.withArgs('http://i.authnomicon.org/oauth2/authorization/http/RedirectURIScheme').returns([ schemeComponent ]);
+      
+      var clients = new Object();
+      clients.read = sinon.stub().yieldsAsync(null, {
+        id: 's6BhdRkqt3',
+        name: 'My Example Client',
+        redirectURIs: [ 'https://client.example.com/cb' ],
+        webOrigins: [ 'https://client.example.com' ]
+      });
+      
+      factory(evaluate, clients, server, authenticate, state, session, parseCookies, logger, container)
+        .then(function(handler) {
+          chai.express.use(handler)
+            .request(function(req, res) {
+              req.query = {
+                client_id: 's6BhdRkqt3',
+                redirect_uri: 'storagerelay://https/client.example.com?id=auth304970'
+              };
+            })
+            .finish(function() {
+              expect(clients.read).to.have.been.calledOnceWith('s6BhdRkqt3');
+              expect(this.req.oauth2.client).to.deep.equal({
+                id: 's6BhdRkqt3',
+                name: 'My Example Client',
+                redirectURIs: [ 'https://client.example.com/cb' ],
+                webOrigins: [ 'https://client.example.com' ]
+              });
+              expect(this.req.oauth2.redirectURI).to.equal('storagerelay://https/client.example.com?id=auth304970');
+              expect(this.req.oauth2.webOrigin).to.equal('https://client.example.com');
+          
+              expect(this.statusCode).to.equal(302);
+              expect(this.getHeader('Location')).to.equal('/consent');
+              done()
+            })
+            .listen();
+        })
+        .catch(done);
+    }); // should evaluate request that uses a redirect URI scheme
+    
   }); // handler
   
 });
