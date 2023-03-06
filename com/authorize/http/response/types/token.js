20899464ccb3d02ca7d2df1489d7e94d02a9b8b0
Jared Hanson <jaredhanson@gmail.com>
2023-03-06 06:22:31 -0800

    Clean up.

--- com/authorize/http/response/types/token.js
+++ com/authorize/http/response/types/token.js
@@ -1,25 +1,24 @@
+// Module dependencies.
+var oauth2orize = require('oauth2orize');
+
 exports = module.exports = function(ats, logger, C) {
-  var oauth2orize = require('oauth2orize');
-  
-  
-  // TODO: require('oauth2orize-idpiframerm')
   
   return Promise.resolve(null)
     .then(function() {
-      var modes = {};
+      var responders = {};
     
       return new Promise(function(resolve, reject) {
         var components = C.components('module:oauth2orize.Responder')
-          , key;
+          , mode;
     
         (function iter(i) {
           var component = components[i];
           if (!component) {
-            return resolve(modes);
+            return resolve(responders);
           }
         
-          key = component.a['@mode'];
-          if (key == 'query') {
+          mode = component.a['@mode'];
+          if (mode == 'query') {
             // The default response mode of this response type is the fragment
             // encoding.  In accordance with security considerations, this
             // response type must not use query encoding, in order to avoid
@@ -31,28 +30,29 @@ exports = module.exports = function(ats, logger, C) {
           }
         
           component.create()
-            .then(function(mode) {
-              logger.info("Loaded response mode '" + key +  "' for OAuth 2.0 implicit grant");
-              modes[key] = mode;
+            .then(function(responder) {
+              logger.info("Loaded response mode '" + mode +  "' for OAuth 2.0 implicit grant");
+              responders[mode] = responder;
               iter(i + 1);
             }, function(err) {
               var msg = 'Failed to load response mode for OAuth 2.0 authorization code grant:\n';
               msg += err.stack;
               logger.warning(msg);
-              return iter(i + 1);
+              iter(i + 1);
             })
         })(0);
       });
     })
-    .then(function(modes) {
-      return [ modes ];
+    .then(function(responders) {
+      // TODO: Load response parameter extension plugins
+      
+      return [ responders ];
     })
     .then(function(plugins) {
-      var modes = plugins[0];
-      var extensions = plugins[1];
+      var responders = plugins[0];
       
       return oauth2orize.grant.token({
-        modes: modes
+        modes: responders
       }, function(client, user, ares, areq, locals, cb) {
         var msg = {};
         // TODO: Eliminate this or move this to locals?
