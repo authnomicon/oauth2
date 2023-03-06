2b532e81a17ca6cba6e0c354bc864e766b2c8bd2
Jared Hanson <jaredhanson@gmail.com>
2023-03-06 07:16:49 -0800

    Clean up tests.

--- test/token/http/grant/code.test.js
+++ test/token/http/grant/code.test.js
@@ -40,64 +40,80 @@ describe('token/http/grant/code', function() {
     });
     
     factory(null, null, logger, container)
-      .then(function(exchange) {
+      .then(function(handler) {
         expect(codeSpy).to.be.calledOnce;
         done();
       })
       .catch(done);
   }); // should create handler without response parameter extensions
 
+
   describe('default behavior', function() {
-    var container = new Object();
-    container.components = sinon.stub()
-    container.components.withArgs('module:@authnomicon/oauth2.tokenResponseParametersFn').returns([]);
+    var acs = new Object();
+    var ats = new Object();
     
-    it('should issue access token', function(done) {
-      var codeSpy = sinon.stub();
-      var factory = $require('../../../../com/token/http/grant/code', {
-        'oauth2orize': { exchange: { code: codeSpy } }
-      });
-      
-      var acs = new Object();
+    var issue;
+    
+    beforeEach(function(done) {
+      var container = new Object();
+      container.components = sinon.stub()
+      container.components.withArgs('module:@authnomicon/oauth2.tokenResponseParametersFn').returns([]);
       acs.verify = sinon.stub().yieldsAsync(null, {
         client: { id: 's6BhdRkqt3' },
         redirectURI: 'https://client.example.com/cb',
         user: { id: '248289761001' }
       });
-      var ats = new Object();
       ats.issue = sinon.stub().yieldsAsync(null, '2YotnFZFEjr1zCsicMWpAA');
       
+      var codeSpy = sinon.stub();
+      var factory = $require('../../../../com/token/http/grant/code', {
+        'oauth2orize': {
+          exchange: { code: codeSpy }
+        }
+      });
+      
       factory(ats, acs, logger, container)
-        .then(function(exchange) {
-          var client = {
+        .then(function(handler) {
+          expect(codeSpy).to.be.calledOnce;
+          
+          issue = codeSpy.getCall(0).args[0];
+          done();
+        })
+        .catch(done);
+    });
+    
+    it('should issue access token', function(done) {
+      var client = {
+        id: 's6BhdRkqt3',
+        name: 'My Example Client',
+        redirectURIs: [ 'https://client.example.com/cb' ]
+      };
+      
+      issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.com/cb', {}, {}, function(err, token) {
+        if (err) { return done(err); }
+    
+        expect(acs.verify).to.be.calledOnceWith('SplxlOBeZQQYbYS6WxSbIA');
+        expect(ats.issue).to.be.calledOnceWith({
+          user: {
+            id: '248289761001'
+          },
+          client: {
             id: 's6BhdRkqt3',
             name: 'My Example Client',
             redirectURIs: [ 'https://client.example.com/cb' ]
-          };
-          
-          var issue = codeSpy.getCall(0).args[0];
-          issue(client, 'SplxlOBeZQQYbYS6WxSbIA', 'https://client.example.com/cb', {}, {}, function(err, token) {
-            if (err) { return done(err); }
-        
-            expect(acs.verify).to.be.calledOnce;
-            expect(acs.verify.getCall(0).args[0]).to.equal('SplxlOBeZQQYbYS6WxSbIA');
-            expect(ats.issue).to.be.calledOnce;
-            expect(ats.issue.getCall(0).args[0]).to.deep.equal({
-              user: {
-                id: '248289761001'
-              },
-              client: {
-                id: 's6BhdRkqt3',
-                name: 'My Example Client',
-                redirectURIs: [ 'https://client.example.com/cb' ]
-              }
-            });
-            expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
-            done();
-          });
-        })
-        .catch(done);
+          }
+        });
+        expect(token).to.equal('2YotnFZFEjr1zCsicMWpAA');
+        done();
+      });
     }); // should issue access token
+    
+  }); // default behavior
+
+  describe('default behavior x', function() {
+    var container = new Object();
+    container.components = sinon.stub()
+    container.components.withArgs('module:@authnomicon/oauth2.tokenResponseParametersFn').returns([]);
     
     it('should issue access token with scope', function(done) {
       var codeSpy = sinon.stub();
