4b064c1336bfd8da70db6496325f69231056d9e3
Jared Hanson <jaredhanson@gmail.com>
2023-03-22 11:39:23 -0700

    WIP: transaction store.

--- test/http/transactionstore.test.js
+++ test/http/transactionstore.test.js
@@ -28,57 +28,6 @@ describe('transactionstore', function() {
   describe('TransactionStore', function() {
     var store = new TransactionStore();
     
-    describe('#load', function() {
-      
-      it('should load transaction', function(done) {
-        var req = new Object();
-        req.state = {
-          client: {
-            id: 's6BhdRkqt3',
-            name: 'My Example Client'
-          },
-          redirectURI: 'https://client.example.com/cb',
-          request: {
-            type: 'code',
-            clientID: 's6BhdRkqt3',
-            redirectURI: 'https://client.example.com/cb',
-            state: 'xyz'
-          }
-        };
-        
-        store.load(req, function(err, txn) {
-          if (err) { return done(err); }
-          
-          expect(txn).to.deep.equal({
-            client: {
-              id: 's6BhdRkqt3',
-              name: 'My Example Client'
-            },
-            redirectURI: 'https://client.example.com/cb',
-            req: {
-              type: 'code',
-              clientID: 's6BhdRkqt3',
-              redirectURI: 'https://client.example.com/cb',
-              state: 'xyz'
-            }
-          });
-          done();
-        });
-      }); // should load transaction
-      
-      it('should error when state middleware is not in use', function(done) {
-        var req = new Object();
-        
-        store.load(req, function(err, txn) {
-          expect(err).to.be.an.instanceOf(Error);
-          expect(err.message).to.equal('OAuth 2.0 requires state support. Did you forget to use `flowstate` middleware?');
-          expect(txn).to.be.undefined;
-          done();
-        });
-      }); // should error when state middleware is not in use
-      
-    }); // #load
-    
     describe('#update', function() {
       
       it('updating transaction', function(done) {
--- test/unit/transactionstore.test.js
+++ test/unit/transactionstore.test.js
@@ -5,6 +5,135 @@ var TransactionStore = require('../../lib/transactionstore');
 describe('TransactionStore', function() {
   var store = new TransactionStore();
   
+  describe('#load', function() {
+    
+    it('should load transaction', function(done) {
+      var req = new Object();
+      req.state = {
+        client: {
+          id: 's6BhdRkqt3',
+          name: 'My Example Client'
+        },
+        redirectURI: 'https://client.example.com/cb',
+        request: {
+          type: 'code',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com/cb',
+          state: 'xyz'
+        }
+      };
+      
+      store.load(req, function(err, txn) {
+        if (err) { return done(err); }
+        
+        expect(txn).to.deep.equal({
+          client: {
+            id: 's6BhdRkqt3',
+            name: 'My Example Client'
+          },
+          redirectURI: 'https://client.example.com/cb',
+          req: {
+            type: 'code',
+            clientID: 's6BhdRkqt3',
+            redirectURI: 'https://client.example.com/cb',
+            state: 'xyz'
+          }
+        });
+        done();
+      });
+    }); // should load transaction
+    
+    it('should load transaction with web origin', function(done) {
+      var req = new Object();
+      req.state = {
+        client: {
+          id: 's6BhdRkqt3',
+          name: 'My Example Client'
+        },
+        webOrigin: 'https://client.example.com',
+        request: {
+          type: 'token',
+          responseMode: 'web_message',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com',
+          state: 'xyz'
+        }
+      };
+      
+      store.load(req, function(err, txn) {
+        if (err) { return done(err); }
+        
+        expect(txn).to.deep.equal({
+          client: {
+            id: 's6BhdRkqt3',
+            name: 'My Example Client'
+          },
+          webOrigin: 'https://client.example.com',
+          req: {
+            type: 'token',
+            responseMode: 'web_message',
+            clientID: 's6BhdRkqt3',
+            redirectURI: 'https://client.example.com',
+            state: 'xyz'
+          }
+        });
+        done();
+      });
+    }); // should load transaction with web origin
+    
+    it('should load transaction with both redirect URI and web origin', function(done) {
+      var req = new Object();
+      req.state = {
+        client: {
+          id: 's6BhdRkqt3',
+          name: 'My Example Client'
+        },
+        redirectURI: 'https://client.example.com/cb',
+        webOrigin: 'https://client.example.com',
+        request: {
+          type: 'token',
+          responseMode: 'web_message',
+          clientID: 's6BhdRkqt3',
+          redirectURI: 'https://client.example.com',
+          state: 'xyz'
+        }
+      };
+      
+      store.load(req, function(err, txn) {
+        if (err) { return done(err); }
+        
+        expect(txn).to.deep.equal({
+          client: {
+            id: 's6BhdRkqt3',
+            name: 'My Example Client'
+          },
+          redirectURI: 'https://client.example.com/cb',
+          webOrigin: 'https://client.example.com',
+          req: {
+            type: 'token',
+            responseMode: 'web_message',
+            clientID: 's6BhdRkqt3',
+            redirectURI: 'https://client.example.com',
+            state: 'xyz'
+          }
+        });
+        done();
+      });
+    }); // should load transaction with both redirect URI and web origin
+    
+    it('should yield error when state middleware is not in use', function(done) {
+      var req = new Object();
+      
+      store.load(req, function(err, txn) {
+        expect(err).to.be.an.instanceOf(Error);
+        expect(err.message).to.equal('OAuth 2.0 requires state support. Did you forget to use `flowstate` middleware?');
+        expect(txn).to.be.undefined;
+        done();
+      });
+    }); // should yield error when state middleware is not in use
+    
+  }); // #load
+  
   describe('#store', function() {
     
     it('should push state', function(done) {
